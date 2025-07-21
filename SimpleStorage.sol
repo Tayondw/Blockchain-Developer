cd0891d06a1267fcc397929b62da9e712a724558
Tayondw <tayondw@gmail.com>
2025-07-21 13:44:33 -0400

    start solidity smart contract

---
+++ SimpleStorage.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+// Solidity, programming language, primarily used for Ethereum Virtual Machine (EVM) based smart contract development
+pragma solidity ^0.8.19; // stating our version of solidity compiler - use keyword "pragma"
+
+// declare the name of the smart contract - use keyword "contract"
+// similar to classes in JS and Python, and structs in Rust
+// compiling a contract means it converts the Solidity code into bytecode and ABI that can be understood and executed by the Ethereum Virtual Machine (EVM).
+contract SimpleStorage {
+
+}
